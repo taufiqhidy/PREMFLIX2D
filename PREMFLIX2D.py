264503466c2105add40507f32b93155245486b57
taufiqhidy <46922081+taufiqhidy@users.noreply.github.com>
2021-08-12 17:22:04 +0700

    Update PREMFLIX2D.py

--- PREMFLIX2D.py
+++ PREMFLIX2D.py
@@ -24,9 +24,21 @@ try:
 except ImportError:
     os.system('pip2 install request')
     time.sleep(1)
-    os.system('Then type: python PREMFILX.py')
+    os.system('Then type: python PREMFILX2D.py')
 
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
 
 def keluar():
         print("Terima kasih telah menggunakan tool ini")
@@ -129,15 +141,16 @@ print("\033[1;92mWELCOME TO MILZU&FIRDOOS")
 def lisensi():
     os.system('clear')
     logine()
-#====premfilx=====#
-def premfilx():
+#====logine=====#
+def logine():
     os.system('clear')
-    print("logo1")
-    print("\033[1;96m[1]\x1b[1;93mStart cloning ( no logine )")
+    print logo1
+    print "\033[1;96m[1]\x1b[1;93mStart cloning ( no logine )"
     time.sleep(0.05)
-    print('\x1b[1;95m[0]\033[1;92m Exit (Coming Soon)')
-    pilih_login()
-def pilih_login():
+    print '\x1b[1;95m[0]\033[1;92m Exit (Coming Soon)'
+    pilih_logine()
+
+def pilih_logine():
     peak = raw_input("\n\033[1;93mCHOOSE: \033[1;91m")
     if peak =="":
         print("\x1b[1;95mFill In Correctly")
@@ -301,5 +314,4 @@ def action():
 
 
 if __name__=="__main__":
-     premfilx()
      login()
