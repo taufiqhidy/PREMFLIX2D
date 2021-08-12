7f784229422ebe26db04b827941548b726ca0edc
taufiqhidy <46922081+taufiqhidy@users.noreply.github.com>
2021-08-12 19:44:21 +0700

    Update PREMFLIX2D.py

--- PREMFLIX2D.py
+++ PREMFLIX2D.py
@@ -96,6 +96,7 @@ print(""")
 "\033[1;95m          M&F»M&F»M&F»M&F»M&F»M&F»M&F»"
 "\033[1;96m          WHATSAPP NUMBER +62 31 2776 8957      ++966 54 399 2329"
 
+os.system("clear")
 print("logo1")
 "\033[1;91m              __________      ________      ________      "
 "\033[1;92m            /    ____   /    /   ___  \   /  ______/     ______   "
@@ -105,6 +106,7 @@ print("logo1")
 "\033[1;96m                  PREMIUM                  PREMIUM"
 "\033[1;97m      PREMIRE                  PREMIRE                  PREMIRE"
 
+os.system ("clear")
 print("logo2")
 "\033[1;97m               ____      ____         _____________                                              "
 "\033[1;91m              /    \    /    \       |   ________ /                                              "
